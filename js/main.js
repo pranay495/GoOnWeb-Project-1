3baa1fb324cf1b866be10c5b695455b10efbe410
pranay495 <pranaymanchineela@gmail.com>
2019-12-11 02:44:55 +0530

    4

--- js/main.js
+++ js/main.js
@@ -10,31 +10,48 @@ function initMap() {
 
 $(document).ready(function() {
   $(".slider").slider({ full_width: true });
+  $('.sidenav').sidenav();
+  // $("ul.tab").click(function(){
+  //   $(#tab-cont:not(this).hide();
+  //   $(this).show();
+  // });
+  // $(".tab-cont").hide();
+
+  $(document).ready(function(){
+    $('ul.tabs').tabs({
+      swipeable : true,
+      // responsiveThreshold : 1920
+    });
+  });
 });
 
 $(document).ready(function(){
-  $('.sidenav').sidenav();
-});
+    $('.carousel-slide').carousel();
+    $('.carousel-slide').carousel('duration','2000');
+
+  });
 
 
-$('#homecarousel').carousel({
-  interval:2000
-})
 
 
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+// function openCity(evt, cityName) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+//
+// // Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//
+// $(document).ready(function(){
+//
+// });
