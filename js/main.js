452e0d60edd82ea764555df4b286742ffb939184
pranay495 <pranaymanchineela@gmail.com>
2019-12-11 22:49:14 +0530

    5

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ function initMap() {
 $(document).ready(function() {
   $(".slider").slider({ full_width: true });
   $('.sidenav').sidenav();
+
+
   // $("ul.tab").click(function(){
   //   $(#tab-cont:not(this).hide();
   //   $(this).show();
@@ -25,11 +27,25 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function(){
-    $('.carousel-slide').carousel();
-    $('.carousel-slide').carousel('duration','2000');
+var slideIndex = 0;
+showSlides();
 
-  });
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 
 
